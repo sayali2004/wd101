883ac5f8b3d315f11355e26a8af1ef8c5286d776
sayali2004 <93052981+sayali2004@users.noreply.github.com>
2023-03-13 19:53:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,110 @@
+const registrationForm = document.getElementById("registrationForm");
+
+const dataTable = document
+
+.getElementById("dataTable") .getElementsByTagName("tbody")[0];
+
+registrationForm.addEventListener("submit", function (e) e.preventDefault();
+
+const name = document.getElementById("name").value; const email = document.getElementById("email").value;
+
+const dob= document.getElementById("dob").value; const password=document.getElementById("password").value;
+
+const terms = document.getElementById("terms").checked;
+
+const today = new Date();
+
+const dobDate = new Date(dob);
+
+const age = today.getFullYear() dobDate.getFullYear();
+
+// Check if age is between 18 and 55 if (age<18 || age > 55) { alert("Age should be between 18 and 55 years."); return; }
+
+// Save form data to localstorage
+
+localstorage.setItem("name", name); localstorage.setItem("email", email);
+
+localstorage.setItem("dob", dob);
+
+localstorage.setItem("password", password);
+
+localstorage.setItem("terms", terms);
+
+// Add form data to table
+
+const row = dataTable.insertRow(); const nameCell = row. insertCell();
+
+const emailcell = row. insertCell();
+
+const dobCell = row. insertCell();
+const passwordcell=row.insertCell();
+const termscell=row.insertCell();
+nameCell.innerHTML = name;
+
+emailcell.innerHTML email;
+
+dobCell.innerHTML dob; passwordcell.innerHTML=password;
+
+termscell.innerHTML=terms;
+
+// Reset form fields registrationForm.reset();
+
+alert("Registration successful.");
+
+// Retrieve form data from localstorage const savedName= localstorage.getItem("name");
+
+const savedEmail = localstorage.getItem("email");
+
+const savedDob= localstorage.getItem("dob"); const savedPassword = localstorage.getItem("password");
+
+const savedTerms = localstorage.getItem("terms");
+
+// Display saved form data in table
+
+if (savedName && savedEmail && savedDob && savedPassword && savedTerms) {
+
+const row = dataTable.insertRow(); const nameCell = row. insertCell();
+
+const emailcell = row. insertCell();
+
+const dobCell = row. insertCell(); 
+const passwordcell=row.insertCell();
 
 
-    function publishToTable() {
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const pass = document.getElementById('pass').value;
-        const date = document.getElementById('date').value;
-        const conditions = document.getElementById('conditions').value;
-        const error = document.getElementById('error');
-        error.innerHTML = (!name || !email) ? 'Name & Email both values are required.' : '';
-        if (name && email) {
-            const tableElement = document.getElementById('table');
-            const trElement = document.createElement('tr');
-            const tbodyElement = document.createElement('tbody');
-            const nameEle = document.createElement('td');
-            const emailEle = document.createElement('td');
-            const passEle = document.createElement('td');
-            const dateEle = document.createElement('td');
-            const conditionsEle = document.createElement('td');
-            nameEle.innerHTML = name;
-            emailEle.innerHTML = email;
-            passEle.innerHTML = pass;
-            dateEle.innerHTML = date;
-            conditionsEle.innerHTML = conditions;
-            trElement.appendChild(nameEle);
-            trElement.appendChild(emailEle);
-            trElement.appendChild(passEle);
-            trElement.appendChild(dateEle);
-            trElement.appendChild(conditionsEle);
-            tbodyElement.appendChild(trElement);
-            tableElement.appendChild(tbodyElement);
-           
+// Retrieve form data from localstorage
 
-    }
-    }
-    
+const savedName = localstorage.getItem("name");
+
+const savedEmail = localstorage.getItem("email");
+
+const savedDob= localstorage.getItem("dob");
+
+const savedPassword localstorage.getItem("password");
+
+const savedTerms = localstorage.getItem("terms");
+
+// Display saved form data in table
+
+if (savedName && savedEmail && savedDob && savedPassword && savedTerms) {
+
+const row dataTable.insertRow();
+
+const nameCell row. insertCell();
+
+const emailcell row. insertCell();
+
+const dobCell row. insertCell();
+
+const passwordcell-row. insertCell();
+
+const termscell-row.insertcell();
+
+nameCell.innerHTML savedName;
+
+emailcell.innerHTML savedEmail;
+
+dobCell.innerHTML savedDob;
+
+passwordcell.innerHTML=-savedPassword; 
+    termscell.innerHTML=savedTerms;
+}
