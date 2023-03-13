a8d1365d642d2bdda7016f9a5977a88c5d26549f
sayali2004 <93052981+sayali2004@users.noreply.github.com>
2023-03-13 18:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 
-function passid_validation(pass,mx,my)
-{
-var pass_len = pass.value.length;
-if (pass_len == 0 ||pass_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-pass.focus();
-return false;
-}
-return true;
-}
