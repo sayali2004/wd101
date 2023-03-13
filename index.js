91c1a1e8fa0f2b3a3b7c7b4116142f6e5a0f6816
sayali2004 <93052981+sayali2004@users.noreply.github.com>
2023-03-13 21:45:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,122 +1,77 @@
 const registrationForm = document.getElementById("registrationForm");
+    const dataTable = document
+      .getElementById("dataTable")
+      .getElementsByTagName("tbody")[0];
 
-const dataTable = document
+    registrationForm.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-.getElementById("dataTable") 
-.getElementsByTagName("tbody")[0];
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const dob = document.getElementById("dob").value;
+      const password = document.getElementById("password").value;
+      const terms = document.getElementById("terms").checked;
 
-registrationForm.addEventListener("submit", function (e){
- e.preventDefault();
+      const today = new Date();
+      const dobDate = new Date(dob);
+      const age = today.getFullYear() - dobDate.getFullYear();
 
-const name = document.getElementById("name").value; 
-const email = document.getElementById("email").value;
+      // Check if age is between 18 and 55
+      if (age < 18 || age > 55) {
+        alert("Age should be between 18 and 55 years.");
+        return;
+      }
 
-const dob= document.getElementById("dob").value; 
- const password=document.getElementById("password").value;
+      // Save form data to localStorage
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("dob", dob);
+      localStorage.setItem("password", password);
+      localStorage.setItem("terms", terms);
 
-const terms = document.getElementById("terms").checked;
-
-const today = new Date();
-
-const dobDate = new Date(dob);
-
-const age = today.getFullYear()- dobDate.getFullYear();
-
-// Check if age is between 18 and 55 
-if (age<18 || age > 55) { 
-  alert("Age should be between 18 and 55 years.");
-  return; 
-}
-
-// Save form data to localstorage
-
-localstorage.setItem("name", name); 
-localstorage.setItem("email", email);
-
-localstorage.setItem("dob", dob);
-
-localstorage.setItem("password", password);
-
-localstorage.setItem("terms", terms);
-
-// Add form data to table
-
-const row = dataTable.insertRow(); 
-const nameCell = row.insertCell();
-
-const emailcell = row.insertCell();
-
-const dobCell = row.insertCell();
-const passwordcell=row.insertCell();
-const termscell=row.insertCell();
-nameCell.innerHTML = name;
-
-emailcell.innerHTML=email;
-
-dobCell.innerHTML=dob; 
-passwordcell.innerHTML=password;
-
-termscell.innerHTML=terms;
-
-// Reset form fields registrationForm.reset();
-
-alert("Registration successful.");
-
-// Retrieve form data from localstorage 
-const savedName= localstorage.getItem("name");
-
-const savedEmail = localstorage.getItem("email");
-
-const savedDob= localstorage.getItem("dob"); const savedPassword = localstorage.getItem("password");
-
-const savedTerms = localstorage.getItem("terms");
-
-// Display saved form data in table
-
-if (savedName && savedEmail && savedDob && savedPassword && savedTerms) {
-
-const row = dataTable.insertRow(); const nameCell = row. insertCell();
-
-const emailcell = row. insertCell();
-
-const dobCell = row. insertCell(); 
-const passwordcell=row.insertCell();
+      // Add form data to table
+      const row = dataTable.insertRow();
+      const nameCell = row.insertCell();
+      const emailCell = row.insertCell();
+      const dobCell = row.insertCell();
+      const passwordcell=row.insertCell();
+      const termscell=row.insertCell();
 
 
-// Retrieve form data from localstorage
+      nameCell.innerHTML = name;
+      emailCell.innerHTML = email;
+      dobCell.innerHTML = dob;
+      passwordcell.innerHTML=password;
+      termscell.innerHTML=terms;
 
-const savedName = localstorage.getItem("name");
 
-const savedEmail = localstorage.getItem("email");
+      // Reset form fields
+      registrationForm.reset();
 
-const savedDob= localstorage.getItem("dob");
+      alert("Registration successful.");
+    });
 
-const savedPassword localstorage.getItem("password");
+    // Retrieve form data from localStorage
+    const savedName = localStorage.getItem("name");
+    const savedEmail = localStorage.getItem("email");
+    const savedDob = localStorage.getItem("dob");
+    const savedPassword = localStorage.getItem("password");
+    const savedTerms = localStorage.getItem("terms");
 
-const savedTerms = localstorage.getItem("terms");
+    // Display saved form data in table
+    if (savedName && savedEmail && savedDob && savedPassword && savedTerms) {
+      const row = dataTable.insertRow();
+      const nameCell = row.insertCell();
+      const emailCell = row.insertCell();
+      const dobCell = row.insertCell();
+      const passwordcell=row.insertCell();
+      const termscell=row.insertCell();
 
-// Display saved form data in table
 
-if (savedName && savedEmail && savedDob && savedPassword && savedTerms) {
 
-const row=dataTable.insertRow();
-
-const nameCell=row. insertCell();
-
-const emailcell=row. insertCell();
-
-const dobCell=row. insertCell();
-
-const passwordcell=row. insertCell();
-
-const termscell=row.insertcell();
-
-nameCell.innerHTML=savedName;
-
-emailcell.innerHTML=savedEmail;
-
-dobCell.innerHTML savedDob;
-
-passwordcell.innerHTML=savedPassword; 
-termscell.innerHTML=savedTerms;
-}
+      nameCell.innerHTML = savedName;
+      emailCell.innerHTML = savedEmail;
+      dobCell.innerHTML = savedDob;
+      passwordcell.innerHTML=savedPassword;
+      termscell.innerHTML=savedTerms;
+    }
